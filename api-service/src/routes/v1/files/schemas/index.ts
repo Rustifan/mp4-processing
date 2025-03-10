20eb6d4a2ef1a78039e625cab2e84be114e675d7
Home <home@Ivans-MacBook-Pro.local>
2025-03-11 00:57:47 +0100

    Fix error fastify schema

--- api-service/src/routes/v1/files/schemas/index.ts
+++ api-service/src/routes/v1/files/schemas/index.ts
@@ -3,7 +3,7 @@ import { Type } from "@sinclair/typebox";
 export const errorResponseSchema = Type.Object(
     {
         statusCode: Type.Number({ description: "status code" }),
-        error: Type.Number({ description: "error type" }),
+        error: Type.String({ description: "error type" }),
         message: Type.String({ description: "Error message" }),
     },
     { description: "Error response" },
